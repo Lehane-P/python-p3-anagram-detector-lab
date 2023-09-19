c286838d85a15eb6f5d6b18e94b05a98856afc08
Lehane-P <Lehane-P>
2023-09-19 13:50:44 +0300

    Done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,29 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        
+        word = self.word.lower()
+
+        
+        anagrams = [w for w in word_list if self.is_anagram(word, w.lower())]
+
+        
+        anagrams = [w for w in anagrams if w != self.word.lower()]
+
+        return anagrams
+
+    @staticmethod
+    def is_anagram(word1, word2):
+       
+        return sorted(word1) == sorted(word2)
+
+
+# test case
+listen = Anagram("listen")
+matches = listen.match(['enlists', 'google', 'inlets', 'banana'])
+
+print(matches) 
